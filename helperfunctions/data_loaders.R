054d262246f6ea4213ac5a0bac7b516f32fcec12
friesandmayo <friesandmayo1.0@gmail.com>
2025-04-16 19:46:52 +0200

    helper functions cleanup for volatilty

---
+++ helperfunctions/data_loaders.R
@@ -0,0 +1,106 @@
+# This file contains all functions related downloading and then importing 
+#chunks of data
+
+#Required libraries:
+library(here)
+library(dplyr)
+library(vroom)
+library(alphavantager)
+#don't forget to load your API key for alphavantage
+
+#--------------------------------------------------------------------------------
+#-----------------                     1                        -----------------
+#--------------------------------------------------------------------------------
+
+# This function loops AlphaVantage's data calls 
+
+#--------------------------------------------------------------------------------
+
+#input year as a number, and symbol as a string 
+data_loop = function(year,symbol){
+  year = paste(year)
+  months = c("01","02","03","04","05","06","07","08","09","10","11","12")
+  for (t in 1:length(months)) {
+    date = paste(year, months[t], sep="-")
+    data = av_get(symbol, av_fun="TIME_SERIES_INTRADAY",interval="1min",
+                  adjusted="false", extended_hours="false",
+                  month=date, outputsize="full")
+    filename = paste(symbol,date,sep="-")
+    filename = paste("~/", filename, sep="")
+    filename = paste(filename,".csv",sep="")
+    write.csv(data,filename)
+  }}
+
+#example: data_loop(2025,"SPY)
+#will download all intraday day for 2025 as a csv table
+#note that this cvs file will go to your documents folder
+
+
+
+#--------------------------------------------------------------------------------
+#-----------------                     2                        -----------------
+#--------------------------------------------------------------------------------
+
+# This function loads every month of a symbol's datasets separately
+
+#--------------------------------------------------------------------------------
+
+#input year as number
+#input months as span, i.e. 1:3 would be months 01 to 03
+#input symbol as string, e.g. "SPY"
+
+data_loader_months = function(year,months,symbol){
+  year = paste(year) #convert to string
+  months = sprintf("%02d",months) #convert to string and add 0 if need
+  for (t in 1:length(months)) {
+    date = paste(year, months[t], sep="-")
+    Rdate = paste(year, months[t], sep="_")
+    dataname = paste(symbol,date,sep="-")
+    filename = paste(dataname,".csv",sep="")
+    Rdataname = paste(symbol,Rdate,sep="_")
+    assign(paste("raw",Rdataname,sep="_"),
+           read.csv(here("data/market_data", filename)), envir = .GlobalEnv)
+  }}
+
+#example: data_loader(year=2021,months=1:6,symbol="SPY")
+#this would import all csv files for 2021, for each month from january to june
+
+
+
+#--------------------------------------------------------------------------------
+#-----------------                     3                        -----------------
+#--------------------------------------------------------------------------------
+
+# This function loads every month of a symbol's datasets and binds them to 
+# have one dataset per symbol per year
+
+#--------------------------------------------------------------------------------
+
+#input year as number
+#input months as span, i.e. 1:3 would be months 01 to 03
+#input symbol as string, e.g. "SPY"
+
+data_loader = function(year,months,symbol){
+  paths <- here("data", "market_data", paste(symbol, 
+                                             sprintf("%04d-%02d", year, months), sep = "-"))
+  
+  files <- paste0(paths, ".csv")
+  
+  combined_data <- bind_rows(vroom(files))
+  
+  assign(paste("raw",symbol,year,sep="_"), combined_data, envir = .GlobalEnv)
+}
+
+#example: data_loader2(year=2021,months=1:6,symbol="SPY")
+#this would import all csv files for 2021, for each month from january to june
+#and combine them into one dataset
+
+
+
+
+
+
+
+
+
+
--- helperfunctions/r.vol_calculators.R
+++ helperfunctions/r.vol_calculators.R
@@ -25,7 +25,7 @@ r.vol_day = function(data){
   return(v_t) #t as the day
 }
 
-#example: r.vol(day_SPY_0402)
+#example: r.vol_day(day_SPY_0402)
 #outputs the realised volatility for the 2nd of April
 
 
@@ -64,24 +64,7 @@ r.vol_day_hour = function(data){
   return(r_vol)
 }
 
-#example: r.vol_hour(day_SPY_0402)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#example: r.vol_day_hour(day_SPY_0402)
 
 
 
@@ -89,44 +72,105 @@ r.vol_day_hour = function(data){
 #-----------------                     3                        -----------------
 #--------------------------------------------------------------------------------
 
+# This function loops the r.vol function in order to compute a vector of 
+# a whole month's daily realised volatility
 
+#--------------------------------------------------------------------------------
 
+#data as a csv file containing a month of data
+r.vol_month = function(data){
+  days = unique(format(as.Date(data$timestamp, #find all days excluding weekends etc
+                               format = "%Y-%m-%d %H:%M:%S"),format="%d"))
+  month = unique(format(as.Date(data$timestamp, #for day selector
+                                format = "%Y-%m-%d %H:%M:%S"),format="%m"))
+  month = as.numeric(month) #due to how day selector input is defined
+  year = unique(format(as.Date(data$timestamp, #for day selector
+                               format = "%Y-%m-%d %H:%M:%S"),format="%Y"))
+  year = as.numeric(year)
+  r_vol = c(1:length(days)) #preparing solution vector
+  
+  for (i in 1:length(days)){ #loop for each day
+    daydata = day_selector(data,year,month,as.numeric(days[i])) #selects data for each day
+    r_vol[i] = r.vol_day(daydata) #computes realized volatility for each day
+  }
+  return(r_vol)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#e.g. r.vol_month(raw_SPY2021_01)
 
 
 #--------------------------------------------------------------------------------
 #-----------------                     4                        -----------------
 #--------------------------------------------------------------------------------
 
+# This function loops the r.vol function in order to compute a vector of 
+# a month's daily realised volatility, for each hour of each day
 
+#--------------------------------------------------------------------------------
 
+#data as a csv file containing only 1 month of data
+r.vol_month_hour = function(data){
+  
+  #prepare dates
+  ndays = unique(format(as.Date(data$timestamp, #find all days excluding weekends etc
+                                format = "%Y-%m-%d %H:%M:%S"),format="%d"))
+  nmonth = unique(format(as.Date(data$timestamp, #for day selector
+                                 format = "%Y-%m-%d %H:%M:%S"),format="%m"))
+  nmonth = as.numeric(nmonth) #due to how day selector input is defined
+  nyear = unique(format(as.Date(data$timestamp, #for day selector
+                                format = "%Y-%m-%d %H:%M:%S"),format="%Y"))
+  nyear = as.numeric(nyear)
+  
+  #take the first day to count how many hours the market is open
+  ndaydata = day_selector(data,nyear,nmonth,as.numeric(ndays[1]))
+  nhours = unique(format(as.POSIXct(ndaydata$timestamp, 
+                                    format = "%Y-%m-%d %H:%M:%S"),format="%H"))
+  
+  #prepare solution matrix, columns are days, rows are hours
+  r_vol_h = matrix(c(1:length(days)),length(nhours),length(ndays))
+  
+  for (j in 1:length(ndays)){ #loop for each day
+    daydata = day_selector(data,nyear,nmonth,as.numeric(ndays[j])) #selects data for each day
+    r_vol_h[,j] = t(r.vol_day_hour(daydata)) #computes realized volatility for each day
+  }
+  return(r_vol_h)
+}
 
-
-
-
-
-
-
+#e.g. r.vol_month_hour(raw_SPY2021_01)
 
 
 #--------------------------------------------------------------------------------
 #-----------------                     5                        -----------------
 #--------------------------------------------------------------------------------
 
+# This function loops the r.vol function in order to compute a vector of 
+# a whole year's daily realised volatility
 
+#--------------------------------------------------------------------------------
+
+#data as a csv file containing a year of data
+r.vol_year = function(data){
+  mmonths = unique(format(as.Date(data$timestamp, #find all months
+                                  format = "%Y-%m-%d %H:%M:%S"),format="%m"))
+  myear = unique(format(as.Date(data$timestamp, #for day selector
+                                format = "%Y-%m-%d %H:%M:%S"),format="%Y"))
+  myear = as.numeric(myear)
+  
+  #columns are months, rows are days
+  r_vol_m = matrix(NA,31,length(mmonths)) #preparing solution matrix
+  
+  for (m in 1:length(mmonths)){ #loop for each month
+    monthdata = month_selector(data,myear,as.numeric(mmonths[m])) #selects data for each month
+    mdays =  unique(format(as.Date(monthdata$timestamp, 
+                                   format = "%Y-%m-%d %H:%M:%S"),format="%d"))
+    mdays = as.numeric(mdays)
+    month_vol = t(r.vol_month(monthdata))
+    r_vol_m[,m] = replace(r_vol_m[,m],mdays,month_vol) #computes realized volatility for each month
+  }
+  return(r_vol_m)
+}
+
+#e.g. r.vol_year(raw_SPY_2024)
 
 
 
