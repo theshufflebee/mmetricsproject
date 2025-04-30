d7dc25d4ae06778ed021dcfa4719ecf8cdde8784
friesandmayo <friesandmayo1.0@gmail.com>
2025-04-30 15:35:48 +0200

    ms compounding tweets n truths

--- helperfunctions/fulldata_loader.R
+++ helperfunctions/fulldata_loader.R
@@ -1,11 +1,7 @@
-# 1. Load Political 
+# 1. Load Political Social Media
 
-#twitter
-tweets <- read.csv(here("data/mothership", "tweets.csv"))
-
-#truthsocial
-truths <- read.csv(here("data/mothership", "truths.csv"))
-
+#contains posts from Twitter & TruthSocial
+social <- read.csv(here("data/mothership", "social.csv"))
 
 # 2. Load Financial
 
@@ -30,19 +26,17 @@ mothership <- data.frame(timestamp = seq(as.POSIXct("2019-01-01"),
 SPY$timestamp = as.POSIXct(SPY$timestamp,format = "%Y-%m-%d %H:%M:%S")
 VGK$timestamp = as.POSIXct(VGK$timestamp,format = "%Y-%m-%d %H:%M:%S")
 ASHR$timestamp = as.POSIXct(ASHR$timestamp,format = "%Y-%m-%d %H:%M:%S")
-tweets$timestamp = as.POSIXct(tweets$timestamp,format = "%Y-%m-%d %H:%M:%S")
-truths$timestamp = as.POSIXct(truths$timestamp,format = "%Y-%m-%d %H:%M:%S")
+social$timestamp = as.POSIXct(social$timestamp,format = "%Y-%m-%d %H:%M:%S")
 
 
 #merge with financial and socialmedia
 mothership = left_join(mothership, SPY, by = "timestamp")
 mothership = left_join(mothership, VGK, by = "timestamp")
 mothership = left_join(mothership, ASHR, by = "timestamp")
-mothership = left_join(mothership, tweets, by = "timestamp")
-mothership = left_join(mothership, truths, by = "timestamp")
+mothership = left_join(mothership, social, by = "timestamp")
 
 
 
 
 #remove intermediary steps
-rm(tweets,truths,SPY,VGK,ASHR)
+rm(social,SPY,VGK,ASHR)
