1a2e35ffd7a7314bdf8534ba02bfb2122dbcab44
friesandmayo <friesandmayo1.0@gmail.com>
2025-04-30 14:16:12 +0200

    new script to load all data

---
+++ helperfunctions/fulldata_loader.R
@@ -0,0 +1,49 @@
+# 1. Load Political 
+
+#truthsocial
+truths <- read.csv(here("data/mothership", "truths.csv"))
+
+#twitter
+tweets <- read.csv(here("data/mothership", "tweets.csv"))
+
+
+# 2. Load Financial
+
+#S&P500
+SPY <- read.csv(here("data/mothership", "SPY.csv"))
+
+#STOXX50
+VGK <- read.csv(here("data/mothership", "VGK.csv"))
+
+#CSI 300 (China)
+ASHR <- read.csv(here("data/mothership", "ASHR.CSV"))
+
+
+# 3. Merge
+
+#mothership base (dataframe with each minute since 2010)
+mothership <- data.frame(timestamp = seq(as.POSIXct("2019-01-01"),
+                                         as.POSIXct("2020-04-16"), 
+                                         by=(1*60)))
+
+#make posixct
+SPY$timestamp = as.POSIXct(SPY$timestamp,format = "%Y-%m-%d %H:%M:%S")
+VGK$timestamp = as.POSIXct(VGK$timestamp,format = "%Y-%m-%d %H:%M:%S")
+ASHR$timestamp = as.POSIXct(ASHR$timestamp,format = "%Y-%m-%d %H:%M:%S")
+tweets$timestamp = as.POSIXct(tweets$timestamp,format = "%Y-%m-%d %H:%M:%S")
+truths$timestamp = as.POSIXct(truths$timestamp,format = "%Y-%m-%d %H:%M:%S")
+
+
+#merge with financial and socialmedia
+mothership = left_join(mothership, SPY, by = "timestamp")
+mothership = left_join(mothership, VGK, by = "timestamp")
+mothership = left_join(mothership, ASHR, by = "timestamp")
+mothership = left_join(mothership, tweets, by = "timestamp")
+mothership = left_join(mothership, truths, by = "timestamp")
+
+
+
+
+#remove intermediary steps
+int_list = ls(tweets,truths,SPY,VGK,ASHR)
+
