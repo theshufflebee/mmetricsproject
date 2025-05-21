45d0196bbad4cee5e8e4bf0d3b156b67150d67e6
friesandmayo <friesandmayo1.0@gmail.com>
2025-05-21 15:52:29 +0200

    final paper

--- helperfunctions/armax_functions.R
+++ helperfunctions/armax_functions.R
@@ -252,10 +252,9 @@ irf.plot = function(model,T, title="ARMA-X IRF"){
     
     plot = ggplot(IRF,aes(x = Period, y = IRFdata)) +
             geom_line(color = "steelblue", size = 1.2) +
-            labs(title = title, y="Hours after shock") +
-            theme_minimal(base_size = 14) +
-            theme(axis.text.x = element_text(angle = 45, hjust = 1),
-            plot.title = element_text(face = "bold", hjust = 0.5))
+            labs(title = title, x="Hours after shock", y="") +
+            theme_minimal() +
+            theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
   return(plot)}
 
